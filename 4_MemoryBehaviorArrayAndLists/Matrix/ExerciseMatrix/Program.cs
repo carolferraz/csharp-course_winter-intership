eb67d15ab280a2edf68cddfaf3b40a2d7eeb7645
Carol Ferraz <caroline.ferraz@eldorado.org.br>
2023-07-03 16:03:45 -0300

    Feat: add Matrix session

---
+++ 4_MemoryBehaviorArrayAndLists/Matrix/ExerciseMatrix/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Globalization;
+
+namespace ExerciseMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two number to be the rows and columns in the matrix: ");
+            string[] vector = Console.ReadLine().Split(' ');
+            int lines = int.Parse(vector[0]);
+            int columns = int.Parse(vector[1]);
+
+            int[,] matrix = new int[lines, columns];
+
+            for (int i = 0; i < lines; i++)
+            {
+                Console.WriteLine($"Enter {columns} numbers:");
+                string[] values = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < columns; j++)
+                {
+                    matrix[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            Console.WriteLine("Enter a number to search into the matrix:");
+            int searchNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (matrix[i, j] == searchNumber)
+                    {
+                        Console.WriteLine("Position: " + i + "," + j);
+                        if (j > 0)
+                        Console.WriteLine($"Left: {matrix[i, j-1]}");
+                        
+                        else if (j < columns - 1)
+                        Console.WriteLine($"Right: {matrix[i,j+1]}");
+                        
+                        else if (i > 0)
+                        Console.WriteLine($"Up: {matrix[i-1,j]}");
+
+                        else if (i < lines - 1)
+                        Console.WriteLine($"Down: {matrix[i+1,j]}");
+                    }
+                    
+                }
+            }
+        }
+    }
+}
---
+++ 4_MemoryBehaviorArrayAndLists/Matrix/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número para definir a ordem da matriz:");
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+
+            for (int i = 0; i < n; i++) //Um for para percorrer o número de linhas n.
+            {
+                Console.WriteLine("Digite três números, separados por espaço, para preencher a linha:");
+                string[] values = Console.ReadLine().Split(' '); //Aqui ele recebe os dados e salva num array. Será criada uma linha da matriz a cada volta do for.
+
+                for (int j = 0; j < n; j++) // Um segundo for para percorrer o número de colunas, nesse caso, tbm n.
+                    matrix[i, j] = int.Parse(values[j]); //Aqui ele salva cada um dos dados da linha que foi salva no array 'values'. Com base no index da linha 'i' e da coluna 'j' ele armazena os valores no array multidimensional 'matrix'.
+
+                {
+
+                }
+            }
+
+            Console.WriteLine("Main diagonal:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(matrix[i, i] + " "); //Percorre a matriz identificando a diagonal principal (matrix[0,0], matrix [1,1], ...), a partir da quantidade de elementos existe nessa diagonal.
+            }
+
+
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matrix[i, j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Total of negative numbers: {count}");
+
+        }
+    }
+}
